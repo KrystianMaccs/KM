16f41d3f5910d19029e29146760c3cf0a7300f93
Krystian Maccs <krystianmaccs@gmail.com>
2023-01-03 03:06:37 +0100

    feat: fixed view rendering

--- apps/portfolio/urls.py
+++ apps/portfolio/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
-from apps.portfolio.views import *
+from . import views
 
 
 app_name = "portfolio"
 
 urlpatterns = [
-    path('portfolios/', PortfolioListView.as_view(), name='portfolio_list'),
-    path('portfolios/<slug:slug>/', PortfolioDetailView.as_view(), name='portfolio_detail'),
-    path('portfolios/create/', PortfolioCreateView.as_view(), name='portfolio_create'),
-    path('portfolios/<slug:slug>/update/', PortfolioUpdateView.as_view(), name='portfolio_update'),
-    path('portfolios/<slug:slug>/delete/', PortfolioDeleteView.as_view(), name='portfolio_delete'),
-    path('categories/', CategoryListView.as_view(), name='category_list'),
-    path('categories/<slug:slug>/', CategoryDetailView.as_view(), name='category_detail'),
-    path('categories/create/', CategoryCreateView.as_view(), name='category_create'),
-    path('categories/<slug:slug>/update/', CategoryUpdateView.as_view(), name='category_update'),
-    path('categories/<slug:slug>/delete/', CategoryDeleteView.as_view(), name='category_delete'),
+    path('portfolios/', views.PortfolioListView.as_view(), name='portfolio_list'),
+    path('portfolios/<int:pk>/', views.PortfolioDetailView.as_view(), name='portfolio_detail'),
+    path('portfolios/create/', views.PortfolioCreateView.as_view(), name='portfolio_create'),
+    path('portfolios/<slug:slug>/update/', views.PortfolioUpdateView.as_view(), name='portfolio_update'),
+    path('portfolios/<slug:slug>/delete/', views.PortfolioDeleteView.as_view(), name='portfolio_delete'),
+    path('categories/', views.CategoryListView.as_view(), name='category_list'),
+    path('categories/<int:pk>/', views.CategoryDetailView.as_view(), name='category_detail'),
+    path('categories/create/', views.CategoryCreateView.as_view(), name='category_create'),
+    path('categories/<slug:slug>/update/', views.CategoryUpdateView.as_view(), name='category_update'),
+    path('categories/<slug:slug>/delete/', views.CategoryDeleteView.as_view(), name='category_delete'),
 ]
--- apps/portfolio/views.py
+++ apps/portfolio/views.py
@@ -4,50 +4,50 @@ from apps.portfolio.models import Category, Portfolio
 
 class CategoryListView(ListView):
     model = Category
-    template_name = 'category_list.html'
+    template_name = 'portfolio/category_list.html'
     context_object_name = 'categories'
 
 class CategoryDetailView(DetailView):
     model = Category
-    template_name = 'category_detail.html'
+    template_name = 'portfolio/category_detail.html'
     context_object_name = 'category'
 
 class CategoryCreateView(CreateView):
     model = Category
     #form_class = CategoryForm
-    template_name = 'category_create.html'
+    template_name = 'portfolio/category_create.html'
 
 class CategoryUpdateView(UpdateView):
     model = Category
     #form_class = CategoryForm
-    template_name = 'category_update.html'
+    template_name = 'portfolio/category_update.html'
 
 class CategoryDeleteView(DeleteView):
     model = Category
-    template_name = 'category_delete.html'
+    template_name = 'portfolio/category_delete.html'
     success_url = '/categories/'
 
 class PortfolioListView(ListView):
     model = Portfolio
-    template_name = 'portfolio_list.html'
+    template_name = 'portfolio/portfolio_list.html'
     context_object_name = 'portfolios'
 
 class PortfolioDetailView(DetailView):
     model = Portfolio
-    template_name = 'portfolio_detail.html'
+    template_name = 'portfolio/portfolio_detail.html'
     context_object_name = 'portfolio'
 
 class PortfolioCreateView(CreateView):
     model = Portfolio
     #form_class = PortfolioForm
-    template_name = 'portfolio_create.html'
+    template_name = 'portfolio/portfolio_create.html'
 
 class PortfolioUpdateView(UpdateView):
     model = Portfolio
     #form_class = PortfolioForm
-    template_name = 'portfolio_update.html'
+    template_name = 'portfolio/portfolio_update.html'
 
 class PortfolioDeleteView(DeleteView):
     model = Portfolio
-    template_name = 'portfolio_delete.html'
+    template_name = 'portfolio/portfolio_delete.html'
     success_url = '/portfolios/'
