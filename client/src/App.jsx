e00b43c2e349496898c5d575e865bbf09465b4c9
Krystian Maccs <krystianmaccs@gmail.com>
2023-02-12 03:20:03 +0100

    client side fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,16 @@
-import './App.css';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CategoryPage from "./pages/CategoryPage";
 import ProjectListPage from "./pages/ProjectListPage";
 import ProjectDetailPage  from './pages/ProjectDetailPage';
+import './App.css';
 
-
-
-const App = () => {
-	return (
-		<>
-			<Router>
-          <Route path="/categories/" element={<CategoryPage />} />
-          <Route path="/project_list/" element={<ProjectListPage />} />
-          <Route path="/str:slug/" element={<ProjectDetailPage />} />
-			</Router>
-		</>
-	);
-};
+const App = () => (
+  <Router>
+    <Route exact path="/categories/" component={CategoryPage} />
+    <Route exact path="/projects_list/" component={ProjectListPage} />
+    <Route exact path="/project/:slug" component={ProjectDetailPage} />
+  </Router>
+);
 
 export default App;
-
